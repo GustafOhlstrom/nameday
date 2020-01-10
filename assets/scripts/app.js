3fa8606f235f4d0453b71af71e87ac080bdae67f
Gustaf Ohlström <gustaf_ohlstrom@hotmail.com>
2020-01-10 16:36:12 +0100

    Added feature displayByName and added all available countries to drop down

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,7 +16,7 @@ const setUrl = () => {
     const country = document.querySelector('#country').value;
     
     //return if no value and if name is less than 3 characters long, api need 3 or more chars 
-    if(!search && search.lenght < 3) return;
+    if(!search || search.lenght < 3) return;
 
     switch(document.querySelector('#category').value) {
         case 'name':
@@ -29,10 +29,38 @@ const setUrl = () => {
     }
 }
 
+const displayByName = resp => {
+    const search = document.querySelector('#search').value;
+    const searchUpperCase = (search.charAt(0).toUpperCase() +search.slice(1))
+
+    resp.results.forEach(days => {
+
+        const date = new Date().getFullYear() + "-" + days.month + "-" + days.day;
+        const names = days.name.split(", ");
+        let searchFoundHTML = "";
+
+        names.forEach((name,i) => {
+            if(name === search || name === searchUpperCase) {
+                searchFoundHTML = `<p class="mb-1">${name}</p>`;
+                names.splice(i, 1);
+            }
+        });
+        let nameString = names.join(', ');
+
+        document.querySelector("#result").innerHTML += `
+            <p class="mb-1">${date}</p>
+            ${searchFoundHTML}
+            <p class="mb-4 other-names">${nameString}</p>
+        `;  
+    });
+};
+
 const display = resp => {
+    console.log(resp);
+    document.querySelector("#result").innerHTML = "";
     switch(document.querySelector('#category').value) {
         case 'name':
-            console.log(resp.results);
+            displayByName(resp);
             return;
         case 'date':
             console.log(resp.data);
