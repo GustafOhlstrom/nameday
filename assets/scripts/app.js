d7695dd7dad112560ff19b199d828f2cb531bb49
Gustaf Ohlström <gustaf_ohlstrom@hotmail.com>
2020-01-10 14:17:20 +0100

    Added getNameday feature with fixed url

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
-
-
+// Update search bar depending on category selected
 const updateSearchBar = () => {
     document.querySelector('#category').value == "name" ? 
         document.querySelector("#search").type = "name": 
@@ -7,6 +6,16 @@ const updateSearchBar = () => {
 }
 updateSearchBar();
 
-document.querySelector('#category').addEventListener('change', function(e) {
-    updateSearchBar();
+document.querySelector('#finder').addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log(e);
+
+    getNameday()
+        .then(resp => {
+            console.log(resp);
+            console.log(resp.data[0].namedays);
+        })
+        .catch(err =>{
+            console.error("An error occured", err);
+        });
 });
--- assets/scripts/nameday-finder.js
+++ assets/scripts/nameday-finder.js
@@ -1,2 +1,5 @@
-
+const getNameday = async () => {
+    const response = await fetch('https://api.abalin.net/namedays?month=7&day=15');
+    return await response.json();
+};
 
